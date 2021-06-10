0b3c7bbd54bf39bfa39d98145143e9683920b318
Abhishek-Roy-Roy <abhishekroyctg2@gmail.com>
2021-06-10 03:03:56 -0700

    my 2nd practice

---
+++ condition.js
@@ -0,0 +1,120 @@
+var con= Math.random()
+var con2=8.564654;
+var con2=Math.ceil(con2)
+
+console.log(con)
+console.log(con2)
+
+var Nam= " He is the best player in the world"
+console.log(Nam.indexOf('best'))
+
+// condition 
+//  if/ else/ if else /break/
+
+var dec= 50;
+var dec2=70;
+if( dec==dec2) {
+console.log("I am the best")
+}
+else{
+    console.log("I am better")
+}
+
+
+// operator//***
+//
+
+var pain=true;
+var deprassion= false;
+if(pain==true && deprassion==true){
+    console.log("You have to fall down")
+}
+else{
+    console.log("You don't need to fall down")
+}
+
+
+var web=100000;
+var crack=60000;
+
+if( web<=100000     ){
+
+    console.log("You can join our course")
+
+}
+else{
+    console.log("You cannot join our course")
+}
+
+var hack="Facebook"
+  var hack2="Instragram"
+
+
+  if(hack=="Facebook" || hack==30  ){
+      console.log(" Go to hell   ")
+  }
+
+  else if(hack2="Instragram"){
+      console.log(" Go to world")
+  }
+
+
+  else{
+      console.log(" GO to heaven")
+  }
+
+var traffic= "green";
+var traffic= "red"
+if("traffic!=green"){
+    console.log(" You have to stop")
+}
+else if("traffic==red"){
+    console.log(" You can move")
+}
+
+var pizza=800;
+var berger=600;
+
+if(pizza!=500 && berger!=600  ){
+    console.log(" You can eat it")
+}
+else{
+    console.log(" Go and bring some money")
+}
+
+var date= new Date()
+console.log(date)
+
+
+var kali= new Date("1999.09.11")
+console.log(kali)
+
+
+alert("") // for alert to open the website
+
+prompt("") // write something  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
