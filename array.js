4e965fe9fefeadf5921eab1f785b9d53b8673cf3
Abhishek-Roy-Roy <abhishekroyctg2@gmail.com>
2021-06-12 23:50:05 -0700

    third practice

---
+++ array.js
@@ -0,0 +1,31 @@
+
+
+ var number= 100;
+ var Number2=[1,2,3,4,5,6,7,8,9,10];
+ console.log(Number2);
+
+
+ var roll=20;
+ var Name=[ "Karim","Rahim","jisan","Janbi","Priya","kat","Eshan" , "Karim","Rahim","jisan","Janbi","Priya","kat","Eshan"];
+ Name.push("Abhishe","Roy","Akhil")
+ Name.pop()
+
+ console.log(Name);
+ console.log(Name[4])
+ console.log(Name.length)
+
+ 
+var roll=[10,11,12,13,14,15,16];
+roll.pop(1)
+console.log(roll)
+
+roll.push("869876875687")
+console.log(roll)
+
+
+var ami=["Abhi", "Esan","Karim", "Rahim","jisan"];
+ami.shift()
+ami.unshift("Karim")
+
+var sei=ami.slice(2)
+console.log(ami)
