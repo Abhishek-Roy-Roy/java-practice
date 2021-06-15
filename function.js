79b42ce502e3fdc4f9239c03cc80ee035da264c5
Abhishek-Roy-Roy <abhishekroyctg2@gmail.com>
2021-06-15 08:32:21 -0700

    loop practice

---
+++ function.js
@@ -0,0 +1,43 @@
+function add(){
+
+    var num= 10;
+    var num2=20;
+    var adder= num + num2;
+    return adder;
+}
+console.log( add())
+
+
+
+function marrige(){
+ var he= "Boys";
+ var she="Girls";
+ var total= he + she;
+ return total;
+
+}
+console.log(marrige()) 
+
+
+function left(){
+    var cat = 100; 
+    var rat = 20;
+    var lefter= cat-rat;
+    return lefter;
+}
+console.log(left())
+
+
+
+function divides( Abhishek , roy){
+    return Abhishek * roy;
+}
+console.log(divides(100, 200 ) )
+console.log(divides ( "Priya" , "Riya"))
+
+
+
+
+
+console.log(add())
+console.log(marrige())
---
+++ loop.js
@@ -0,0 +1,31 @@
+var football= 1;
+
+
+while(football < 10){
+    console.log( football)
+     football++;
+}
+
+
+var number= 1;
+while( number <= 5){
+    console.log("Abhishek Roy")
+     ++number;
+
+}
+
+// for loop ***
+
+var num= 1;
+for(num=1; num<=10; num++){
+    console.log(num)
+
+}
+
+var cricket= 10;
+for(  i=1; i<cricket; i++){
+    console.log(i)
+}
+
+
+
